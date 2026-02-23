6418742ccfb774b2424bf4547eae559f88627f67
Gustavo Z. dos Reis <gustavo.zaghi1227@gmail.com>
2026-02-23 12:32:53 -0300

    alterações na query e na construção do Json

--- api/models/veiculoModel.php
+++ api/models/veiculoModel.php
@@ -5,14 +5,57 @@
     //GET methods
     //Consultas retornam veiculo e vagão relacionados
         public static function listarVeiculo ($pdo){
-            $sql = "SELECT * FROM veiculo
-                    INNER JOIN vagao
-                    ON veiculo.idveiculo = vagao.idveiculo
+            $sql = "SELECT veiculo.idveiculo, veiculo.placa, veiculo.tipo, vagao.idvagao, vagao.comprimento, vagao.largura, vagao.altura, funcionario.idfuncionario, funcionario.nomeFunc, funcionario.cpf, funcionario.adm, funcionario.ativo as funcativo FROM veiculo
+                    LEFT JOIN vagao
+                    ON veiculo.idveiculo = vagao.idveiculo AND vagao.ativo = 'ativo'
+                    INNER JOIN funcionario
+                    ON veiculo.idfuncionario = funcionario.idfuncionario
                     WHERE veiculo.ativo = 'ativo'";
             $stmt = $pdo->prepare($sql);
             $stmt->execute();
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+            $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            $objVeiculos = [];
+
+            $veiculos = [];
+            foreach ($dados as $row){
+
+                $id = $row["idveiculo"];
+
+                // Se o veículo ainda não foi criado no array
+                if (!isset($veiculos[$id])) {
+
+                    $veiculos[$id] = [
+                        "idveiculo" => $row["idveiculo"],
+                        "placa" => $row["placa"],
+                        "tipo" => $row['tipo'],
+                        "vagoes" => [], // agora é lista
+                        "funcionario" => [
+                            "idfuncionario" => $row['idfuncionario'],
+                            "nomeFunc" => $row['nomeFunc'],
+                            "cpf" => $row['cpf'],
+                            "adm" => $row['adm'],
+                            "funcativo" => $row['funcativo']
+                        ]
+                    ];
+                }
+
+        // Se existir vagão (LEFT JOIN pode trazer null)
+        if ($row['idvagao'] != null) {
+
+            $veiculos[$id]["vagoes"][] = [
+                "comprimento" => $row['comprimento'],
+                "largura" => $row['largura'],
+                "altura" => $row['altura']
+            ];
         }
+
+         
+    }
+
+    // Remove as chaves associativas (fica array normal)
+    return array_values($veiculos);
+}
 
         public static function buscarPorId($pdo, $idVeiculo){
   
