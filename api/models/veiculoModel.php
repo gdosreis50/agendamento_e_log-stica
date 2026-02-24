043f0a197368b171709f4abb3b8a3027b49331ad
Gustavo Z. dos Reis <gustavo.zaghi1227@gmail.com>
2026-02-24 11:58:35 -0300

    atualização get veículo por ID

--- api/models/veiculoModel.php
+++ api/models/veiculoModel.php
@@ -15,7 +15,6 @@
             $stmt->execute();
             $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-            $objVeiculos = [];
 
             $veiculos = [];
             foreach ($dados as $row){
@@ -40,33 +39,71 @@
                     ];
                 }
 
-        // Se existir vagão (LEFT JOIN pode trazer null)
-        if ($row['idvagao'] != null) {
+                // Se existir vagão (LEFT JOIN pode trazer null)
+                if ($row['idvagao'] != null) {
 
-            $veiculos[$id]["vagoes"][] = [
-                "comprimento" => $row['comprimento'],
-                "largura" => $row['largura'],
-                "altura" => $row['altura']
-            ];
+                    $veiculos[$id]["vagoes"][] = [
+                        "comprimento" => $row['comprimento'],
+                        "largura" => $row['largura'],
+                        "altura" => $row['altura']
+                    ];
+                }
+
+                
+            }
+
+            // Remove as chaves associativas (fica array normal)
+            return array_values($veiculos);
         }
-
-         
-    }
-
-    // Remove as chaves associativas (fica array normal)
-    return array_values($veiculos);
-}
 
         public static function buscarPorId($pdo, $idVeiculo){
   
-            $sql = "SELECT * FROM veiculo
-                    INNER JOIN vagao
-                    ON veiculo.idveiculo = vagao.idveiculo
-                    WHERE veiculo.idVeiculo = :idVeiculo  AND veiculo.ativo = 'ativo'";
+            $sql = "SELECT veiculo.idveiculo, veiculo.placa, veiculo.tipo, vagao.idvagao, vagao.comprimento, vagao.largura, vagao.altura, funcionario.idfuncionario, funcionario.nomeFunc, funcionario.cpf, funcionario.adm, funcionario.ativo as funcativo 
+                    FROM veiculo
+                    LEFT JOIN vagao
+                    ON veiculo.idveiculo = vagao.idveiculo AND vagao.ativo = 'ativo'
+                    INNER JOIN funcionario
+                    ON veiculo.idfuncionario = funcionario.idfuncionario
+                    WHERE veiculo.ativo = 'ativo' AND veiculo.idVeiculo = :idVeiculo";
+            
             $stmt = $pdo->prepare($sql);
             $stmt->bindParam(":idVeiculo", $idVeiculo);
             $stmt->execute();
-            return $stmt->fetch(PDO::FETCH_ASSOC);
+            $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+            if(empty($data)){
+                return null;
+            }
+            
+            $veiculo = [
+                "idveiculo" => $data[0]["idveiculo"],
+                "placa" => $data[0]["placa"],
+                "tipo" => $data[0]['tipo'],
+                "vagoes" => [], // agora é lista
+                "funcionario" => [
+                    "idfuncionario" => $data[0]['idfuncionario'],
+                    "nomeFunc" => $data[0]['nomeFunc'],
+                    "cpf" => $data[0]['cpf'],
+                    "adm" => $data[0]['adm'],
+                   "funcativo" => $data[0]['funcativo']
+                ]
+            ];
+                
+
+            // Se existir vagão (LEFT JOIN pode trazer null)
+
+            foreach($data as $row){
+                if ($row['idvagao'] != null) {
+
+                $veiculo["vagoes"][] = [
+                    "comprimento" => $row['comprimento'],
+                    "largura" => $row['largura'],
+                    "altura" => $row['altura']
+                ];
+            }
+            }
+            
+            return ($veiculo);
         }
 
     //POST method
@@ -112,68 +149,67 @@
         }
 
     //PUT method
-    public static function editarVeiculo($data, $idVeiculo, $pdo){
+        public static function editarVeiculo($data, $idVeiculo, $pdo){
 
-        $required = ['placa'];
+            $required = ['placa'];
 
-            foreach ($required as $field) {
-                if (empty($data[$field])) {
-                    http_response_code(400);
+                foreach ($required as $field) {
+                    if (empty($data[$field])) {
+                        http_response_code(400);
+                        return [
+                            "success" => false,
+                            "error" => "Campo obrigatório ausente: $field"
+                        ];
+                    }
+                }
+
+                $sql = "UPDATE veiculo set 
+                                placa = ?,
+                                tipo = ?, 
+                                idFuncionario = ? WHERE idVeiculo = ?";
+
+                try{
+                    $stmt = $pdo->prepare($sql);
+                    $stmt->execute([
+                        $data['placa'],
+                        $data['tipo'],
+                        $data['idfuncionario'],
+                        $idVeiculo
+                    ]);
+
+                    http_response_code(200);
                     return [
-                        "success" => false,
-                        "error" => "Campo obrigatório ausente: $field"
+                        "success" => true,
+                    ];
+                } catch(PDOException $e){
+                    http_response_code(500);
+                    return [
+                        "Success" => false,
+                        "Erro" => $e->getMessage()
                     ];
                 }
-            }
-
-            $sql = "UPDATE veiculo set 
-                            placa = ?,
-                            tipo = ?, 
-                            idFuncionario = ? WHERE idVeiculo = ?";
-
-            try{
-                $stmt = $pdo->prepare($sql);
-                $stmt->execute([
-                    $data['placa'],
-                    $data['tipo'],
-                    $data['idfuncionario'],
-                    $idVeiculo
-                ]);
-
-                http_response_code(200);
-                return [
-                    "success" => true,
-                ];
-            } catch(PDOException $e){
-                http_response_code(500);
-                return [
-                    "Success" => false,
-                    "Erro" => $e->getMessage()
-                ];
-            }
-    }
+        }
 
 
     //DELETE method
+        public static function excluirVeiculo($idVeiculo, $pdo){
 
-    public static function excluirVeiculo($idVeiculo, $pdo){
+            $sql = "UPDATE veiculo SET ativo = 'desativado' WHERE idVeiculo = :idVeiculo";
 
-        $sql = "UPDATE veiculo SET ativo = 'desativado' WHERE idVeiculo = :idVeiculo";
-
-        try{
-            $stmt = $pdo->prepare($sql);
-            $stmt->bindParam(":idVeiculo", $idVeiculo);
-            $stmt->execute();
-            http_response_code(200);
-            return ["success" => true];
-        }catch (PDOException $e){
-            http_response_code(500);
-            return [
-                    "Success" => false,
-                    "Erro" => $e->getMessage()
-                ];
+            try{
+                $stmt = $pdo->prepare($sql);
+                $stmt->bindParam(":idVeiculo", $idVeiculo);
+                $stmt->execute();
+                http_response_code(200);
+                return ["success" => true];
+            }catch (PDOException $e){
+                http_response_code(500);
+                return [
+                        "Success" => false,
+                        "Erro" => $e->getMessage()
+                    ];
+            }
         }
-    }
 
     }
 
